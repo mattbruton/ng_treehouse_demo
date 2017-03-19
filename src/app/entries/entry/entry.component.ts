1c9801c1c0f705b6ab14f8cb33a952825fdfb286
MATT BRUTON <the.bruton@gmail.com>
2017-03-19 18:05:24 -0500

    created files for entry and entry list components

---
+++ src/app/entries/entry/entry.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-entry',
+    templateUrl: './entry.component.html',
+    styleUrls: ['./entry.component.css']
+})
+export class EntryComponent {
+    
+}
