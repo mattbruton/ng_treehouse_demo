7df8bfec330d6e6601994c232040b260caaafa0a
MATT BRUTON <the.bruton@gmail.com>
2017-03-19 18:11:59 -0500

    created barrel for components in entries directory, import components into app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './entries';
+import { EntryComponent } from './entries';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent,
+        EntryListComponent,
+        EntryComponent
+        ],
     bootstrap: [AppComponent]
 })
 
---
+++ src/app/entries/index.ts
@@ -0,0 +1,2 @@
+export * from './entry/entry.component';
+export * from './entry-list/entry-list.component';
